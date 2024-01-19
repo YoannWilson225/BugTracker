54af1e8c3128afdacf2be70872b86320d178473d
yoann-philippe wilson <yoannphilippewilson@gmail.com>
2024-01-19 16:48:51 +0000

    https

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let xhr = new XMLHttpRequest();
-let url = new URL('http://greenvelvet.alwaysdata.net/bugTracker/api');
+let url = new URL('https://greenvelvet.alwaysdata.net/bugTracker/api');
 let userId, userName, userPassword, userToken, title, description;
 const tokenName = 'bugTracker_token';
 
